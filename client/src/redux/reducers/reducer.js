4b57db28c4f71136078c6bcd71cbc46c44fde0ef
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-12-04 22:53:44 +0100

    Fixed redux, add constants/action creators for fetch images

---
+++ client/src/redux/reducers/reducer.js
@@ -0,0 +1,56 @@
+import { FETCH_IMAGES, REQUEST_FETCH_IMAGES } from '../constants';
+
+const requestImagesFetch = () => ({
+    type: REQUEST_FETCH_IMAGES
+})
+
+const imagesFetched = (images) => ({
+    type: FETCH_IMAGES,
+    images
+});
+
+const initialState = {
+    user_images: [{
+        id: 123,
+        name: 'Some cool name',
+        author: 'Patryk',
+        title: 'My first Image',
+        description: 'Something new! Cool image!',
+        imageUrl: 'https://images.pexels.com/photos/3095527/pexels-photo-3095527.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        location: {
+            lat: 51.702372,
+            lng: 19.414996
+        },
+        creator: 'q1'
+    },
+
+    {
+        id: Math.random(),
+        name: 'Some cool name',
+        author: 'Peter',
+        title: 'Some cool image',
+        description: 'Something new! Cool image!',
+        imageUrl: 'https://images.pexels.com/photos/3095521/pexels-photo-3095521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        location: {
+            lat: 51.702372,
+            lng: 19.414996
+        }
+    }],
+
+    users: {
+        id: 'q1',
+        name: 'Patryk',
+        places: 3,
+        image: 'https://images.pexels.com/photos/2987769/pexels-photo-2987769.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        description: 'Hello world! Work is still in progress! :)'
+    }
+}
+
+export const images = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_IMAGES:
+            return []
+        default:
+            return state
+    }
+}
--- client/src/routes/Images/Images.js
+++ client/src/routes/Images/Images.js
@@ -4,33 +4,6 @@ import { useParams } from 'react-router-dom';
 import ImagesList from './components/ImagesList';
 
 const Images = props => {
-    const TEST_IMAGES = [{
-        id: 123,
-        name: 'Some cool name',
-        author: 'Patryk',
-        title: 'My first Image',
-        description: 'Something new! Cool image!',
-        imageUrl: 'https://images.pexels.com/photos/3095527/pexels-photo-3095527.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        location: {
-            lat: 51.702372,
-            lng: 19.414996
-        },
-        creator: 'q1'
-    },
-
-    {
-        id: Math.random(),
-        name: 'Some cool name',
-        author: 'Peter',
-        title: 'Some cool image',
-        description: 'Something new! Cool image!',
-        imageUrl: 'https://images.pexels.com/photos/3095521/pexels-photo-3095521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        location: {
-            lat: 51.702372,
-            lng: 19.414996
-        }
-    }];
-
     const userId = useParams().userId;
     const loadedImages = TEST_IMAGES.filter(image => image.creator === userId);
     return <ImagesList usersImages={loadedImages} />
