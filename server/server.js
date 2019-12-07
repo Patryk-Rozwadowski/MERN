5965032aec7c01b88d3772b7fa293b435b4ef430
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-12-07 17:35:21 +0100

    Add cors, api images

--- server/server.js
+++ server/server.js
@@ -5,3 +5,42 @@ const app = express();
 app.use(cors);
 app.use(express.urlencoded( { extended: false }));
 app.use(express.json());
+
+const port = 8000;
+
+app.get('/api/images', (req, res) => {
+    const data = [
+        {
+            id: 123,
+            name: 'Some cool name',
+            author: 'Patryk',
+            title: 'My first Image',
+            description: 'Something new! Cool image!',
+            imageUrl: 'https://images.pexels.com/photos/3095527/pexels-photo-3095527.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            location: {
+                lat: 51.702372,
+                lng: 19.414996
+            },
+            creator: 'q1'
+        },
+
+        {
+            id: 2,
+            name: 'Some cool name',
+            author: 'Peter',
+            title: 'Some cool image',
+            description: 'Something new! Cool image!',
+            imageUrl: 'https://images.pexels.com/photos/3095521/pexels-photo-3095521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+            location: {
+                lat: 51.702372,
+                lng: 19.414996
+            }
+        }
+    ];
+
+    res.json(data);
+});
+
+app.listen(8000, () => {
+    console.log(`Server is running on port: ${port}`);
+});
