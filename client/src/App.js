49a3518eb074d5867be504dc29c383c8e58258f8
Patryk Rozwadowski <47067438+Patryk-Rozwadowski@users.noreply.github.com>
2019-12-10 11:50:04 +0100

    Split Images for comopnents and index

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import Nav from './shared/Nav/Nav';
 import HomeRoute from './routes/Home/index';
 import SignUp from './routes/SignUp/SignUp';
 import Users from './modules/Users/Users';
-import Images from './routes/Images/Images';
+import ImagesRoute from './routes/Images/index';
 
 const App = () => {
   return (
@@ -20,7 +20,7 @@ const App = () => {
         <Route path='/' exact component={HomeRoute} />
         <Route path='/users' exact component={Users} />
         <Route path='/signup' exact component={SignUp} />
-        <Route path='/images' exact component={Images} />
+        <Route path='/images' exact component={ImagesRoute} />
         <Redirect to='/' />
       </Switch>
     </Router>
---
+++ client/src/routes/Images/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Images from './components/Images';
+
+const ImagesRoute = () => {
+    return (
+        <React.Fragment>
+            <Images />
+        </React.Fragment>
+    )
+};
+
+export default ImagesRoute;
