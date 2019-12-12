37410937768cbb126fcea0c11fb2687605d2aec0
Patryk Rozwadowski <47067438+Patryk-Rozwadowski@users.noreply.github.com>
2019-12-12 16:00:20 +0100

    Add random background for Home

--- client/src/routes/Home/components/Home/Home.js
+++ client/src/routes/Home/components/Home/Home.js
@@ -3,28 +3,48 @@ import React from 'react';
 import './styles/Home.scss';
 import ContainedButtons from '../../../../shared/Buttons/Buttons';
 
-const HomeContainer = {
-    HomeImage: {
-        backgroundImage: 'url(https://source.unsplash.com/random/1600x900/?moon.space,dark,grey,night)',
-        overflow: 'hidden',
-        flex: 1,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
-    },
-};
+class Home extends React.Component {
 
-const Home = () => {
-    return (
-        <React.Fragment>
-            <section style={HomeContainer.HomeImage} className='home__container'>
-                <div className='home__title-wrapper'>
-                    <h1 className='home__title'>Share your photos</h1>
-                    <h2 className='home__subtitle'> Ok, not yet, but i'm working on it! </h2>
-                    <ContainedButtons />
-                </div>
-            </section>
-        </React.Fragment>
-    )
-};
+    constructor() {
+        super();
+        this.state = {
+                   backgroundImage: 'url(https://source.unsplash.com/random/1600x900/?dark,space,moon,grey',
+                   overflow: 'hidden',
+                   flex: 1,
+                   backgroundSize: 'cover',
+                   backgroundRepeat: 'no-repeat',
+            }
+        }
+
+    componentDidMount = () => {
+        debugger;
+        this.interval = setInterval(() =>  this.setState({
+            backgroundImage: `url(https://source.unsplash.com/random/1600x900/?${this.randomPic()})`
+        }), 5000);
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    randomPic = () => {
+        const arr = ['moon', 'space', 'dark', 'grey', 'building', 'stars', 'sky'];
+        return arr[Math.floor(arr.length * Math.random())]
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <section style={this.state} className='home__container'>
+                    <div className='home__title-wrapper'>
+                        <h1 className='home__title'>Share your photos</h1>
+                        <h2 className='home__subtitle'> Ok, not yet, but i'm working on it! </h2>
+                        <ContainedButtons />
+                    </div>
+                </section>
+            </React.Fragment>
+        )
+    }
+}
 
 export default Home;
