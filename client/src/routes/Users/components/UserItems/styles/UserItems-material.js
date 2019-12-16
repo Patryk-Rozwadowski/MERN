6826738ff6619fe1c033b8d73bf20fe1b256815f
Patryk Rozwadowski <47067438+Patryk-Rozwadowski@users.noreply.github.com>
2019-12-16 10:47:52 +0100

    Splitted UserItems styles

---
+++ client/src/routes/Users/components/UserItems/styles/UserItems-material.js
@@ -0,0 +1,9 @@
+import {makeStyles} from "@material-ui/core/styles";
+
+const UserItemsMaterialStyles = makeStyles({
+    card: {
+        maxWidth: 345,
+    },
+});
+
+export default UserItemsMaterialStyles;
