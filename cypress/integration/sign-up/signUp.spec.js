0999124603b58d5b8d5cfdb10d6460c9b530b842
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-01-18 16:15:46 +0100

    c

--- cypress/integration/sign-up/signUp.spec.js
+++
@@ -1,6 +0,0 @@
-describe('signUp / register', () => {
-  it('should be able to add login and password', () => {
-    cy.visit('/signup');
-    cy.get('#email').type('fake@gmail.com');
-  });
-});
--- cypress/utils.js
+++
@@ -1,7 +0,0 @@
-export const getElementAndType = el => {
-  return function(text) {
-    return cy.get(el).type(text);
-  };
-};
-
-export const getElement = el => cy.get(`[data-cy="${el}"]`);
