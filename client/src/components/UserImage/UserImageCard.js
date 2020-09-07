bb96d33f53b0e5a2c30b56f601d34622cfe82964
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-09-08 01:41:39 +0200

    Add styles for images card

--- client/src/components/UserImage/UserImageCard.js
+++ client/src/components/UserImage/UserImageCard.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import starSVG from '../../assets/star.svg';
+import commentSVG from '../../assets/comment.svg';
+import Grid from '@material-ui/core/Grid';
 
 const UserImageCard = ({ imageUrl, author, title }) => {
   let img = new Image();
@@ -14,19 +17,46 @@ const UserImageCard = ({ imageUrl, author, title }) => {
       }}
     >
       <div className='image-card__feed'>
-          
-        <div className='image-card__title'>
-          <h2 className='image-card__title-text'>
-            {title ? title : 'unknown'}
-          </h2>
-        </div>
-        <div className='image-card__description'>
-          <h2 className='image-card__author'>{author ? author : 'unknown'}</h2>
-        </div>
+        <Grid direction={'row'} container>
+          <div className='image-card__image-info'>
+            <h2 className='image-card__title'>{title ? title : 'unknown'}</h2>
+          </div>
 
-        <p className='image-card__info__element'> 5 Faves </p>
-        <p className='image-card__info__element'> 2 Comments </p>
-        <p className='image-card__info__element'> 1601 Views </p>
+          <Grid container direction='row'>
+            <Grid xs={8} item>
+              <h2 className='image-card__author'>
+                by {author ? author : 'unknown'}
+              </h2>
+            </Grid>
+
+            <Grid xs={4} item>
+              <div className='image-card__icons'>
+                <Grid container direction={'row'}>
+                  <Grid xs={6} item>
+                    <Grid container alignContent='center' justify='center'>
+                      <img
+                        className='image-card__icon'
+                        src={starSVG}
+                        alt='star icon'
+                      />
+                      <p className='image-card__icon-text'>24</p>
+                    </Grid>
+                  </Grid>
+                  <Grid xs={6} item>
+                    <Grid container alignContent='center' justify='center'>
+                      <img
+                        className='image-card__icon'
+                        src={commentSVG}
+                        alt='star icon'
+                      />
+                      <p className='image-card__icon-text'>2</p>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
