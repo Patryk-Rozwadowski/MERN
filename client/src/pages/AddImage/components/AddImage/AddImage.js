2a8f52ac25ae85b18f772416fa1675a5a45e8f3b
Patryk Rozwadowski <pm.rozwadowski@gmail.com>
2020-11-15 00:07:05 +0100

    Reformatted code with prettier

--- client/src/pages/AddImage/components/AddImage/AddImage.js
+++ client/src/pages/AddImage/components/AddImage/AddImage.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import UploadImageForm from "../UploadImageForm/UploadImageForm";
-import UploadImageDropBox from "../UploadImageDropBox/UploadImageDropBox";
+import UploadImageForm from '../UploadImageForm/UploadImageForm';
+import UploadImageDropBox from '../UploadImageDropBox/UploadImageDropBox';
 
-const AddImage = () => {
-    return (
-        <Grid alignItems={'center'} direction={'row'} container>
-            <Grid lg={6} item>
-                <UploadImageForm/>
-            </Grid>
-            <Grid lg={4} item>
-                <UploadImageDropBox/>
-            </Grid>
-        </Grid>
-    )
-};
+const AddImage = () => (
+  <Grid alignItems={'center'} direction={'row'} container>
+    <Grid lg={6} item>
+      <UploadImageForm />
+    </Grid>
+    <Grid lg={4} item>
+      <UploadImageDropBox />
+    </Grid>
+  </Grid>
+);
 
 export default AddImage;
--- client/src/pages/UsersList/components/UserCardDescription/UserCardDescription.js
+++ client/src/pages/UsersList/components/UserCardDescription/UserCardDescription.js
@@ -1,16 +1,17 @@
-import Grid from "@material-ui/core/Grid";
-import Heading from "../../../../components/Heading/Heading";
-import Text from "../../../../components/Text/Text";
-import React from "react";
+import Grid from '@material-ui/core/Grid';
+import Heading from '../../../../components/Heading/Heading';
+import Text from '../../../../components/Text/Text';
+import React from 'react';
 
-const UserCardDescription = ({name, description}) =>
-    <Grid justify={'center'} container>
-        <Grid className='text-container' item>
-            <Heading variant='subtitle' color='black' type='h2'>
-                {name}
-            </Heading>
-            <Text>{description ? description : 'No description provided!'}</Text>
-        </Grid>
+const UserCardDescription = ({ name, description }) => (
+  <Grid justify={'center'} container>
+    <Grid className='text-container' item>
+      <Heading variant='subtitle' color='black' type='h2'>
+        {name}
+      </Heading>
+      <Text>{description ? description : 'No description provided!'}</Text>
     </Grid>
+  </Grid>
+);
 
 export default UserCardDescription;
